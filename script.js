bd4f15c40c12f8c98af29d37cd49b62fe78d77e8
frazeinky <rifkyzena26@gmail.com>
2022-11-17 15:52:35 +0700

    final final

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const loader = document.querySelector('.loader');
 
 // NASA API
 const count = 10;
-const apiKey = 'DEMO_KEY';
+const apiKey = 'Lre1TFPdaRaqgfckTizupcYNZNO89oSR0IchzqOg';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
